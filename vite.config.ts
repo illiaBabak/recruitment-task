81a2a6f244dbb5a08c3feffa4ed40517a0ff0a25
Illia Babak <illiababak@MacBook-Air-Illia.local>
2026-07-03 20:45:58 +0200

    chore: initialize project

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  build: {
+    outDir: "build",
+    emptyOutDir: true,
+  },
+  envPrefix: "ENV_",
+  server: {
+    open: true,
+    port: 3000,
+  },
+  plugins: [react(), checker({ typescript: true })],
+  resolve: {
+    alias: {
+      src: "/src",
+    },
+  },
+});
